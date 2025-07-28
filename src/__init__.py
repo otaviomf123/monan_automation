7f00ff47eb3c8bdd111411448fc57002f45ea81c
Otavio M. Feitosa <otaviomf123@gmail.com>
2025-07-28 23:44:32 +0200

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,38 @@
+"""
+MONAN/MPAS Runner
+=================
+
+Sistema automatizado para execução do modelo MONAN/MPAS no ambiente CEMPA.
+
+Módulos:
+    config_loader: Carregamento e gerenciamento de configurações
+    data_downloader: Download de dados GFS do NOAA
+    wps_processor: Processamento WPS (ungrib)
+    initial_conditions: Geração de condições iniciais
+    boundary_conditions: Geração de condições de fronteira
+    model_runner: Execução do modelo MONAN/MPAS
+    utils: Utilitários gerais
+
+Autor: Otavio Feitosa
+Data: 2025
+"""
+
+__version__ = "1.0.0"
+__author__ = "Otavio Feitosa"
+__email__ = "otavio.feitosa@cempa.br"
+
+from .config_loader import ConfigLoader
+from .data_downloader import GFSDownloader
+from .wps_processor import WPSProcessor
+from .initial_conditions import InitialConditionsGenerator
+from .boundary_conditions import BoundaryConditionsGenerator
+from .model_runner import ModelRunner
+
+__all__ = [
+    'ConfigLoader',
+    'GFSDownloader', 
+    'WPSProcessor',
+    'InitialConditionsGenerator',
+    'BoundaryConditionsGenerator',
+    'ModelRunner'
+]
